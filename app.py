b115bf1ce0791b702f33d4d827d9f8c22d8fbe9f
johnymiro <johnymiro93@gmail.com>
2024-02-12 20:59:07 +0000

    added necessary funcs

--- app.py
+++ app.py
@@ -1,15 +1,80 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from dotenv import load_dotenv
 from PyPDF2 import PdfReader
 from langchain.text_splitter import CharacterTextSplitter
 from langchain.embeddings import OpenAIEmbeddings, HuggingFaceInstructEmbeddings
 from langchain.vectorstores import FAISS
 from langchain.chat_models import ChatOpenAI
-from langchain.memory import ConversationBufferMemory
 from langchain.chains import ConversationalRetrievalChain
+from langchain.memory import ConversationBufferMemory
 
 
 app = Flask(__name__)
+
+def get_pdf_text(pdf_docs):
+    text = ""
+    for pdf in pdf_docs:
+        pdf_reader = PdfReader(pdf)
+        for page in pdf_reader.pages:
+            text += page.extract_text()
+    return text
+
+def get_text_chunks(text):
+    text_splitter = CharacterTextSplitter(
+        separator="\n",
+        chunk_size=1000,
+        chunk_overlap=200,
+        length_function=len
+    )
+    chunks = text_splitter.split_text(text)
+    return chunks
+
+def get_vectorstore(text_chunks):
+    embeddings = OpenAIEmbeddings()
+    # embeddings = HuggingFaceInstructEmbeddings(model_name="hkunlp/instructor-xl")
+    vectorstore = FAISS.from_texts(texts=text_chunks, embedding=embeddings)
+    return vectorstore
+
+def get_conversation_chain(vectorstore):
+    llm = ChatOpenAI()
+
+    memory = ConversationBufferMemory(
+        memory_key='chat_history', return_messages=True)
+    print("memory:", memory)
+    conversation_chain = ConversationalRetrievalChain.from_llm(
+        llm=llm,
+        retriever=vectorstore.as_retriever(),
+        memory=memory
+    )
+    
+    return conversation_chain
+
+
+@app.route('/upload-pdf', methods=['POST'])
+def upload_pdf():
+    # Check if the post request has the file part
+    if 'file' not in request.files:
+        return jsonify({"error": "No file part"}), 400
+    file = request.files['file']
+    # If the user does not select a file, the browser submits an
+    # empty file without a filename.
+    if file.filename == '':
+        return jsonify({"error": "No selected file"}), 400
+    if file and file.filename.endswith('.pdf'):
+        raw_text = get_pdf_text(file)
+
+        # get the text chunks
+        text_chunks = get_text_chunks(raw_text)
+
+        # create vector store
+        vectorstore = get_vectorstore(text_chunks)
+
+        return jsonify({"message": "PDF uploaded successfully", "filename": vectorstore}), 200
+    else:
+        return jsonify({"error": "Only PDF files are accepted"}), 400
+
+
+
 
 @app.route('/')
 def home():
@@ -20,10 +85,6 @@ def get_data():
     data = {"name": "John Doe", "age": 30}
     return jsonify(data)
 
-@app.route('/api/ai-pdf')
-def get_data2():
-    data = {"name": "John Doe", "age": 30, "test": ["this", "test"]}
-    return jsonify(data)
 
 if __name__ == '__main__':
     app.run(debug=True)
